e0cd44c9c76d98844a67176250f85e2c237bac27
onlymeknow88 <fadjri.w@gmail.com>
2021-11-01 22:41:57 +0700

    CarouseCategory Commit

--- script.js
+++ script.js
@@ -1,43 +1,64 @@
-(function () {
-  var carouselContent = document.querySelector("#carouselContent");
-  var btnLeft = document.querySelector("#btnLeft");
-  var btnRight = document.querySelector("#btnRight");
+(function (){
+  let currentScrollPosition = 0;
+  let scrollAmount = 180;
+  
+  const sCont = document.querySelector(".tag");
+  const hScroll = document.querySelector(".scroll-container");
+  const btnLeft = document.querySelector("#leftBtn");
+  const btnRight = document.querySelector("#rightBtn");
+  var scrollContainer = document.querySelector(".scroll-container");
+  
+  btnLeft.style.opacity = "0";
+  
+  let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+  
+  btnLeft.addEventListener('click', moveCarouselLeft);
+  btnRight.addEventListener('click', moveCarouselRight);
+  
+  function moveCarouselLeft(){
+      currentScrollPosition += (1 * scrollAmount);
+      
+      if(currentScrollPosition >= 0) {
+          currentScrollPosition = 0;
+          btnLeft.style.opacity = "0";
+          btnLeft.style.pointerEvents = "none";
+      }else {
+          btnLeft.style.opacity = "1";
+          btnLeft.style.pointerEvents = "auto";
+      }
 
-  var carouselLeftValue = 0;
-  var totalWidth = carouselContent.getBoundingClientRect().width;
-  var widthToMove = 180 + 16 + 180 + 16;
-  var arrowBtnWidth = 50;
-  var noOfSlides = totalWidth / widthToMove;
-  var currentSlide = 1;
-
-  btnLeft.addEventListener("click", moveCaraouselRight);
-  btnRight.addEventListener("click", moveCaraouselLeft);
-
-  manageButtons();
-
-  function moveCaraouselLeft() {
-    if (currentSlide == 1) carouselLeftValue -= widthToMove - arrowBtnWidth;
-    else carouselLeftValue -= widthToMove;
-
-    currentSlide++;
-    manageButtons();
-    carouselContent.style.left = `${carouselLeftValue}px`;
+      if(currentScrollPosition <= maxScroll){ 
+          currentScrollPosition = maxScroll; 
+          btnRight.style.opacity = "0" ;
+          btnRight.style.pointerEvents = "none"; 
+      }else { 
+          btnRight.style.opacity = "1" ; 
+          btnRight.style.pointerEvents = "auto"; 
+      }
+      
+      sCont.style.transform="translateX(" + currentScrollPosition + "px)"; 
   }
+  
+  function moveCarouselRight(){
+      currentScrollPosition += (-1 * scrollAmount);
 
-  function moveCaraouselRight() {
-    if (currentSlide == 2) carouselLeftValue = 8;
-    else carouselLeftValue += widthToMove;
-
-    currentSlide--;
-    manageButtons();
-    carouselContent.style.left = `${carouselLeftValue}px`;
-  }
-
-  function manageButtons() {
-    if (currentSlide == noOfSlides - 1) btnRight.classList.remove("show");
-    else btnRight.classList.add("show");
-
-    if (currentSlide == 1) btnLeft.classList.remove("show");
-    else btnLeft.classList.add("show");
-  }
+      if(currentScrollPosition >= 0) {
+        currentScrollPosition = 0;
+        btnLeft.style.opacity = "0";
+        btnLeft.style.pointerEvents = "none";
+      }else {
+          btnLeft.style.opacity = "1";
+          btnLeft.style.pointerEvents = "auto";
+      }
+      
+      if(currentScrollPosition <= maxScroll){ 
+          currentScrollPosition = maxScroll; 
+          btnRight.style.opacity = "0" ;
+          btnRight.style.pointerEvents = "none"; 
+      }else { 
+          btnRight.style.opacity = "1" ; 
+          btnRight.style.pointerEvents = "auto"; 
+      }
+      sCont.style.transform="translateX(" + currentScrollPosition + "px)"; 
+    }
 })();
