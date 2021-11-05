938a5f1e69929708713c71bf6d3270a88310e8b9
onlymeknow88 <fadjri.w@gmail.com>
2021-11-05 23:41:05 +0700

    Filter Carousel Done

---
+++ carousel.js
@@ -0,0 +1,8 @@
+ $('.owl-carousel').owlCarousel({
+    //  loop: false,
+    //  margin: 12,
+    nav: true,      
+    autoWidth:true,
+    navText : ['<div class="left-arrow-button"><div class="button-arrow"><a><button class="btn btn-left"><div class="left-icon"><svg viewBox="0 0 16 16" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="display: block; width: 100%; height: 100%;"><g mirror-in-rtl="" class="style-scope yt-icon"><path d="M10.15,13.35L4.79,8l5.35-5.35l0.71,0.71L6.21,8l4.65,4.65L10.15,13.35z" class="style-scope yt-icon"></path></g></svg></div></button></a></div></div>','<div class="right-arrow-button"><div class="button-arrow"><a><button class="btn btn-next"><div class="next-icon"><svg viewBox="0 0 16 16" preserveAspectRatio="xMidYMid meet" focusable="false" class="style-scope yt-icon" style="display: block; width: 100%; height: 100%;"><g mirror-in-rtl="" class="style-scope yt-icon"><path d="M4.97,12.65L9.62,8L4.97,3.35l0.71-0.71L11.03,8l-5.35,5.35L4.97,12.65z"class="style-scope yt-icon"></path></g></svg></div></button></a></div></div>'],
+    navContainer: '.filter-tags .owl-nav-custom',
+});
